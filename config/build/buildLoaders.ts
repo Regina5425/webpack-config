412382efe0af0495d5d497b5ddcca5c35e3bafdb
Regina <reina5425@gmail.com>
2024-01-01 16:08:07 +0300

    add alias and assest loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,6 +5,34 @@ import {BuildOptions} from './types/types';
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
 	const isDev = options.mode === 'development';
 
+	const assetLoader = {
+		test: /\.(png|jpg|jpeg|gif)$/i,
+		type: 'asset/resource',
+	};
+
+	const svgLoader = {
+		test: /\.svg$/i,
+		issuer: /\.[jt]sx?$/,
+		use: [
+			{
+				loader: '@svgr/webpack',
+				options: {
+					icon: true,
+					svgoConfig: {
+						plugins: [
+							{
+								name: 'convertColors',
+								params: {
+									currentColor: true
+								}
+							}
+						]
+					}
+				},
+			}
+		],
+	};
+
 	const cssLoaderWithModules = {
 		loader: 'css-loader',
 		options: {
@@ -30,6 +58,8 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
 	};
 
 	return [
+		assetLoader,
+		svgLoader,
 		scssLoader,
 		tsLoader
 	];
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import {useState} from 'react';
 import classes from './App.module.scss';
 import {Link, Outlet} from 'react-router-dom';
+import Pic1 from '../assets/2.png';
+import Svg1 from '../assets/3.svg';
 
 export const App = () => {
 	const [counter, setCounter] = useState(0);
@@ -11,6 +13,12 @@ export const App = () => {
 			<br/>
 			<Link to='/shop'>shop</Link>
 			<h1>counter: {counter}</h1>
+			<div>
+				<img width={100} height={100} src={Pic1} alt=""/>
+			</div>
+			<div>
+				<Svg1  width={100} height={100} />
+			</div>
 			<button className={classes.button} onClick={increment}>inc</button>
 			<Outlet />
 		</div>
